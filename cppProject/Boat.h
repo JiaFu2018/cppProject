de305037e1748fd7b1c204074058d1242b55b8c4
JiaFU <jia.fu@etu.unice.fr>
2017-12-24 19:57:13 +0100

    add 3 types of boats

--- cppProject/Boat.h
+++ cppProject/Boat.h
@@ -7,31 +7,33 @@
 //
 #pragma once
 #include "GraphicPrimitives.h"
+#include "Setting.h"
 #include "Star.h"
 
 class Boat {
     
 public:
 
-    Boat(float boatX_,float boatY_, float boatSize_ = 0.05f, int boatType_ =2):
+    Boat(float boatX_,float boatY_, float boatSize_ = 0.05f, int boatType_ =2, bool newBoat_ = true):
     boatX(boatX_),
     boatY(boatY_),
     boatSize(boatSize_),
     boatType(boatType_),
+    newBoat(newBoat_),
     bombX(boatX_),
     bombY(boatY_),
     bombFrequency(0.5f),
     bombSpeed(0.01f){}
-    
+
     float boatX, boatY, boatSize;
     float bombX, bombY, bombFrequency, bombSpeed;
     int boatType, bombType;
+    bool newBoat;
     const double PI = acos(-1.0f);
-    //std::vector<Star *> stars;
     
     void draw();
     void tick(std::vector<Star * >& stars);
+    void drawBoat(int boatType);
     void drawBomb(float bombX, float bombY, int bombType, float bombFrequency, float bombSpeed);
-    //void shootStar(std::vector<Star *> stars);
 
 };
